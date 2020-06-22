9c60bb5871e0c07de6a264651eb34ed213a45900
Chao Yang <cyang05@outlook.com>
2020-06-21 21:03:32 -0500

    Add crystal version support in .env

--- docker/download_lastest_crystal_package.sh
+++ docker/download_lastest_crystal_package.sh
@@ -6,18 +6,22 @@ get_latest_release() {
     sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
 }
 
-latest_tag=$(get_latest_release 'crystal-lang/crystal')
+if [ ${crystal_version} = "latest" ] ; then
+  git_tag=$(get_latest_release 'crystal-lang/crystal')
+else
+  git_tag=${crystal_version}
+fi
 
 if [ $1 = "debian" ] ; then
-  package_name="crystal_${latest_tag}-1_amd64.deb"
+  package_name="crystal_${git_tag}-1_amd64.deb"
   package_path="/tmp/crystal.deb"
 else
-  package_name="crystal-${latest_tag}-1-linux-x86_64.tar.gz"
+  package_name="crystal-${git_tag}-1-linux-x86_64.tar.gz"
   package_path="/tmp/crystal.tar.gz"
 fi
 
-package_url="https://github.com/crystal-lang/crystal/releases/download/${latest_tag}/$package_name"
+package_url="https://github.com/crystal-lang/crystal/releases/download/${git_tag}/$package_name"
 
-echo "latest tag is $latest_tag"
-echo "downloading latest package from $package_url"
+echo "selected crystal tag is $git_tag"
+echo "downloading crystal package from url: $package_url"
 curl -L $package_url -o $package_path
